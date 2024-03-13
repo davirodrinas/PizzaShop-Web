fe924c3b63c1cef954c603f70da4533e20e4a2e9
Davi Rodrigues Do Nascimento Junior <davi.nascimento@linx.com.br>
2024-03-13 08:20:15 -0300

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
+
+export function App() {
+  return (
+    
+    <div className='flex justify-center itens-center h-screen'>
+      <Button>
+        Enviar
+      </Button>
+    </div>
+  )
+}
+
