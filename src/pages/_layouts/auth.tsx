bfb337e2bbc8776de3b21edc06b1670ce16246de
Davi Rodrigues Do Nascimento Junior <davi.nascimento@linx.com.br>
2024-03-14 08:23:37 -0300

    manha14-03

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom"
+import { Pizza } from 'lucide-react'
 
 export function AuthLayout() {
     return(
-        <div>
-            <h1>Autenticação</h1>
-
-            <div>
+        <div className="min-h-screen grid grid-cols-2">
+            <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col junstify-between"> </div>
+            <div className="flex intems-center gap-3 text-lg font-medium text-foreground">
+                <Pizza className="h-5 w5"/>
+                <span className="font-semibold">pizza.shop</span>
+            </div>
+            <footer className="text-sm">
+                Painel do parceiro &copy; pizza.shop - {new Date().getUTCFullYear()}
+            </footer>
+            <div className="flex flex-col itens-center justify-center">
                 <Outlet />
             </div>
         </div>
--- src/pages/aut/sing-in.tsx
+++ src/pages/aut/sing-in.tsx
@@ -1,3 +1,33 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Helmet } from "react-helmet-async"
+
+
 export function SingIn() {
-    return <h1>SingIn</h1>
+    return (
+      <>
+        <Helmet title="Login"/>
+        <div className="p-8">
+            <div className="w-[350px] flex flex-col justify-center gap-6">
+                <div className="flex flex-col gap-2 text-center">
+                    <h1 className="text-2xl font-semibold tracking-tight">
+                        Acessar Painel
+                    </h1 >
+                    <p className="text-sm text-muted-foreground">Acompanhe susa vendas pelo painel do parceiro!</p>
+                </div>
+
+                <form className="flex flex-col space-y-4">
+                    <div className="space-y-2">
+                        <Label htmlFor="email">Seu Email</Label>
+                        <Input className="w-full" id="email" type="email" />
+                    </div>
+
+                    <Button type="submit">Acessar Painel</Button>
+                </form>
+            </div>
+        </div>
+        
+      </>
+    )
 }
