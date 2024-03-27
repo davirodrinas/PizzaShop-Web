f3a99e24b76c601292285cf6f6feb1dbe47ee5fa
Davi Rodrigues Do Nascimento Junior <davi.nascimento@linx.com.br>
2024-03-27 17:34:00 -0300

    tarde27

---
+++ src/components/pagination.tsx
@@ -0,0 +1,27 @@
+export interface PaginationProps {
+  pageIndex: number
+  totalCount: number
+  perPage: number
+}
+
+export function Pagination({
+  pageIndex,
+  perPage,
+  totalCount,
+}: PaginationProps) {
+  const pages = Math.ceil(totalCount / perPage) || 1
+
+  return (
+    <div className="flex items-center justify-between">
+        <span className="text-sm text-muted-foreground">
+            Total de {totalCount} item(s)
+        </span>
+        
+        <div className="flex items-center gap-6 lg:gap-8">
+            <div className="tex-sm font-medium">Página {pageIndex + 1} de {pages} </div>
+            <div>
+                
+        </div>
+    </div>
+  )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,18 +1,42 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet } from 'react-helmet-async'
+
+import { Table, TableBody, TableHead, TableRow } from '@/components/ui/table'
+
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
-    return (
-       <>
-            <Helmet title="Pedidos" />
-            <div className="flex flex-col gap-4">
-                <h1 className="text-3x1 font-bold tracking-tighter"></h1>
-            </div>
-            <div className="space-y-2.5">
-                <form className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <input placeholder='Nome do cliente' className="h-8 w-[320px]"/>
-                </form>
-            </div>
-       </>
-    )
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3x1 font-bold tracking-tighter"></h1>
+      </div>
+      <div className="space-y-2.5">
+        <OrderTableFilters />
+
+        <div className="border rouded-md">
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180x]">Ralizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do Pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return <OrderTableRow key={i} />
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </>
+  )
 }
