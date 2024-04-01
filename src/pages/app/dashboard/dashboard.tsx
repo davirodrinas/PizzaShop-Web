a8c564767d27243059de322ec0ed404594b9aaaf
Davi Rodrigues Do Nascimento Junior <davi.nascimento@linx.com.br>
2024-04-01 20:55:31 -0300

    noite01-04

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,12 +1,22 @@
 import { Helmet } from 'react-helmet-async'
 
+import { MontRevenueCard } from './dashboar-revenue-card'
+import { DayOrdersAmountCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
+import { MonthOrdersAmountCard } from './month-orders-amount-card'
+
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
       <div className="flex flex-col gap-4">
         <h1 className="text-exl font-bold tracking-tight">Dashboard</h1>
-        <div className="grid grid-cols-4 gap-4"></div>
+        <div className="grid grid-cols-4 gap-4">
+          <MontRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
       </div>
     </>
   )
