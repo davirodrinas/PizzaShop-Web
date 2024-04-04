7ea8a73e7de062b657706f89e5b0645bb894d77f
Davi Rodrigues Do Nascimento Junior <davi.nascimento@linx.com.br>
2024-04-04 13:08:24 -0300

    tarde04-04

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,5 +1,5 @@
 import { BarChart } from 'lucide-react'
-import { Pie, PieChart, ResponsiveContainer } from 'recharts'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
@@ -10,6 +10,14 @@ const data = [
   { product: 'marguerita', amount: 50 },
   { product: '4 queijos', amount: 16 },
   { product: 'Frango Frito', amount: 26 },
+]
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
 ]
 
 export function PopularProductsChart() {
@@ -35,8 +43,47 @@ export function PopularProductsChart() {
               outerRadius={86}
               innerRadius={64}
               strokeWidth={8}
-              fill={colors.emerald[500]}
-            />
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product.length > 12
+                      ? data[index].product.substring(0, 12).concat('...')
+                      : data[index].product}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index]}
+                    className="stroke-background"
+                  />
+                )
+              })}
+            </Pie>
           </PieChart>
         </ResponsiveContainer>
       </CardContent>
